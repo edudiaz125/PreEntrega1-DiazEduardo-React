46c7785d5d42ebab6d9fb9deeaad955d30785e0e
Eduardo Diaz <edu.diaz125@gmail.com>
2024-07-24 23:42:01 -0300

    Preentrega1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { ItemListContainer } from "./components/itemListContainer"
+import { NavBar } from "./components/NavBar"
+
+
+function App() {
+
+  return( 
+  <> 
+  <NavBar/>
+  <ItemListContainer greeting ="Bienvenido"/>
+  </>
+  )
+}
+
+export default App
